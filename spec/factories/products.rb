5716ce6a48a77b9d636d861202665dac1b2b4a43
kota-kaneta <foruri@icloud.com>
2020-10-07 14:14:30 +0900

    ルボコップ

--- spec/factories/products.rb
+++ spec/factories/products.rb
@@ -1,11 +1,11 @@
 FactoryBot.define do
-  #factory :product do
-    #content {Faker::Lorem.sentence}
-    #association :user
-    #association :room
+  # factory :product do
+  # content {Faker::Lorem.sentence}
+  # association :user
+  # association :room
 
-    #after(:build) do |item|
-      #item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
-    #end
-  #end
+  # after(:build) do |item|
+  # item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+  # end
+  # end
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,14 +1,14 @@
 FactoryBot.define do
   factory :user do
-    nickname {Faker::Name.initials(number: 4)}
-    first_name {Faker::Name.initials(number: 4)}
-    last_name {Faker::Name.initials(number: 4)}
-    first_name_ruby {Faker::Name.initials(number: 4)}
-    last_name_ruby {Faker::Name.initials(number: 4)}
-    birthday {Faker::Date.in_date_period}
-    email {Faker::Internet.free_email}
+    nickname { Faker::Name.initials(number: 4) }
+    first_name { Faker::Name.initials(number: 4) }
+    last_name { Faker::Name.initials(number: 4) }
+    first_name_ruby { Faker::Name.initials(number: 4) }
+    last_name_ruby { Faker::Name.initials(number: 4) }
+    birthday { Faker::Date.in_date_period }
+    email { Faker::Internet.free_email }
     password = Faker::Internet.password(min_length: 6)
-    password {password}
-    password_confirmation {password}
+    password { password }
+    password_confirmation { password }
   end
 end

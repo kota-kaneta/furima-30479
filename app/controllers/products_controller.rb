25abe6d223981bea38fc897b6a7cd060f236d739
kota-kaneta <foruri@icloud.com>
2020-10-09 18:53:37 +0900

    修正2

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -36,7 +36,7 @@ class ProductsController < ApplicationController
     if @product.destroy
       redirect_to root_path
     else
-      render actin: :show
+      render action: :show
     end
   end
 

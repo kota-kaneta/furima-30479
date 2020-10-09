39e90db752425f7bf14cd21ebe2ee1249f3538c3
kota-kaneta <foruri@icloud.com>
2020-10-09 18:39:43 +0900

    商品削除機能

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -32,6 +32,12 @@ class ProductsController < ApplicationController
     end
   end
 
+  def destroy
+    @product = Product.find(params[:id])
+    @product.destroy
+    redirect_to root_path
+  end
+
   private
 
   def product_params
@@ -43,5 +49,3 @@ class ProductsController < ApplicationController
   end
 
 end
-
-# .valid?
